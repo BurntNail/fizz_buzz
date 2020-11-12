41e223298b7435c09b5fdaf8849549589fa8399f
Epacnoss <49815482+Epacnoss@users.noreply.github.com>
2020-11-12 12:13:02 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,16 @@ fn main() {
 fn doTheFor(max: i32, map: &HashMap<i32, &str>) {
     for n in 1..max {
         let mut out = String::new();
+        let mut works = False;
 
         for (key, value) in map {
             if n % key == 0 {
                 out += &value.to_string()[..];
+                works = True;
             }
         }
 
-        if out == String::new() {
+        if !works {
             println!("{}", n);
         } else {
             println!("{}", out);
