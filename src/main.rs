2023dfe468de2298aeb02245fb684ad86b003826
Jack Maguire <Maguire-J-19@kcs.org.uk>
2020-11-13 14:14:32 +0000

    Allow to run with cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::collections::HashMap;
+use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let n: i32 = args[1].parse().expect("Not a valid 32 bit signed integer.");
+
     println!();
 
     let mut reqs = HashMap::new();
     reqs.insert(3, "Fizz");
     reqs.insert(5, "Buzz");
 
-    doTheFor(100, &reqs);
+    doTheFor(n, &reqs);
 }
 
 fn doTheFor(max: i32, map: &HashMap<i32, &str>) {
